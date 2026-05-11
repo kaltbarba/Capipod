d95a9933666b805f4d5ca66beb1b31dc048ba906
Kenny <kaltbarba@gmail.com>
2026-05-11 15:54:15 -0500

    Basic elements on grid

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -11,16 +11,39 @@ export default function Board({
   buildings: Building[];
   size: number;
 }) {
-  console.log({ pods, buildings, size });
+  const podsMap = new Map<string, Pod>(
+    pods.map((pod) => [`${pod.coordinate.x},${pod.coordinate.y}`, pod]),
+  );
+
+  const buildingsMap = new Map<string, Building>();
+  buildings.forEach((building) =>
+    building.coordinates.forEach((c) =>
+      buildingsMap.set(`${c.x},${c.y}`, building),
+    ),
+  );
+  console.log({ podsMap, buildingsMap });
   return (
     <div className="board-container">
       {Array.from({ length: size }, (_, y) => (
         <div key={y}>
-          {Array.from({ length: size }, (_, x) => (
-            <div key={x} className="board-cell">
-              [{x},{y}]
-            </div>
-          ))}
+          {Array.from({ length: size }, (_, x) => {
+            const pod = podsMap.get(`${x},${y}`);
+            const building = buildingsMap.get(`${x},${y}`);
+            const element = pod || building;
+
+            return (
+              <div
+                key={x}
+                className={[
+                  "board-cell",
+                  building && "building",
+                  pod?.state,
+                ].join(" ")}
+              >
+                {element ? "" : `${x},${y}`}
+              </div>
+            );
+          })}
         </div>
       ))}
     </div>
--- src/data/pods.ts
+++ src/data/pods.ts
@@ -1,17 +1,16 @@
 import Pod from "../classes/Pod";
 
 const pods: Pod[] = [
-  new Pod({ id: 1, name: "Tar Flood", coordinate: { x: 5, y: 5 } }),
-  new Pod({ id: 2, name: "Lizard Mutts", coordinate: { x: 20, y: 5 } }),
-  new Pod({ id: 3, name: "Firestorm", coordinate: { x: 35, y: 5 } }),
-  new Pod({ id: 4, name: "Bullet Rain", coordinate: { x: 50, y: 5 } }),
-  new Pod({ id: 5, name: "Net Trap", coordinate: { x: 5, y: 20 } }),
-  new Pod({ id: 6, name: "Toxic Gas", coordinate: { x: 20, y: 20 } }),
-  new Pod({ id: 7, name: "Spike Wall", coordinate: { x: 35, y: 20 } }),
-  new Pod({ id: 8, name: "Electrified", coordinate: { x: 50, y: 20 } }),
-  new Pod({ id: 9, name: "Freeze Ray", coordinate: { x: 5, y: 35 } }),
-  new Pod({ id: 10, name: "Darkness", coordinate: { x: 20, y: 35 } }),
-  new Pod({ id: 11, name: "Noise Bomb", coordinate: { x: 35, y: 35 } }),
+  new Pod({ id: 1, name: "Tar Flood", coordinate: { x: 2, y: 2 } }),
+  new Pod({ id: 2, name: "Lizard Mutts", coordinate: { x: 8, y: 5 } }),
+  new Pod({ id: 3, name: "Firestorm", coordinate: { x: 15, y: 3 } }),
+  new Pod({ id: 4, name: "Bullet Rain", coordinate: { x: 22, y: 7 } }),
+  new Pod({ id: 5, name: "Net Trap", coordinate: { x: 29, y: 0 } }),
+  new Pod({ id: 6, name: "Toxic Gas", coordinate: { x: 5, y: 14 } }),
+  new Pod({ id: 7, name: "Spike Wall", coordinate: { x: 12, y: 20 } }),
+  new Pod({ id: 8, name: "Electrified", coordinate: { x: 20, y: 25 } }),
+  new Pod({ id: 9, name: "Freeze Ray", coordinate: { x: 27, y: 18 } }),
+  new Pod({ id: 10, name: "Darkness", coordinate: { x: 29, y: 29 } }),
 ];
 
 export default pods;
